474e0b9cef3d850738e2ef2b6351b33146d94639
infrml <84241310+infrml@users.noreply.github.com>
2021-09-08 04:02:21 +0000

    update logo text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = class extends React.Component {
         <div className="container flex-around">
           <div className="navbar-brand logo">
             <Link to="/" className="navbar-item" title="Logo">
-              <h1>Brand | Logo</h1>
+              <h1>Informal | Tech:Life </h1>
             </Link>
               <GitHubButton href="https://github.com/infrml">Follow</GitHubButton>
             {/* Hamburger menu */}
